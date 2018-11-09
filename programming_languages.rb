c04adb8f870f996455ac9f3cf73e5d91ff4d1a18
Yuki N <lunarbase@outlook.com>
2018-11-09 19:10:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,7 @@
 def reformat_languages(languages)
   # your code here
+  
+  new_hash = {}
 end
+
+#https://stackoverflow.com/questions/41493536/iterate-through-nested-hash-to-create-an-array-while-adding-items-to-it
