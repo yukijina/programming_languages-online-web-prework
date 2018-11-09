2053a504eee298afa9c9c34901f44657fbd2277a
Yuki N <lunarbase@outlook.com>
2018-11-09 23:19:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,14 @@
 def reformat_languages(languages)
   # your code here
   
-  new_hash = {}
+  obj = Hash.new {|hash, key| hash[key] = {type: nil, style: []}}
+
+  languages.each_with_object(obj) do |(style,language), item|
+    language.each do |lang, hash_of_type|
+        item[lang][:style] << style
+        item[lang][:type] = hash_of_type[:type] 
+  end
+ end
 end
 
 #https://stackoverflow.com/questions/41493536/iterate-through-nested-hash-to-create-an-array-while-adding-items-to-it
