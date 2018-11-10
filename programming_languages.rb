396f9b1863f7a313dc8168abda5424d3ca3226bd
Yuki N <lunarbase@outlook.com>
2018-11-10 01:27:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+  
+  #type: nil for string
+  obj = Hash.new {|hash, key| hash[key] = {type: nil, style: []}}
+
+  languages.each_with_object(obj) do |(key,value), hash|
+    value.each do |lang, item|
+        hash[lang][:style] << key
+        hash[lang][:type] = item[:type] 
+  end
+ end
 end
+
